40e19886a7d8411e86201a7c77f7f47178d31edb
avinash aaglave <136832484+avinashaag96@users.noreply.github.com>
2023-07-21 11:47:07 +0530

    Add files via upload

---
+++ Controllers/BookingDetailsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FleetManagement.Model;
+
+namespace FleetManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingDetailsController : ControllerBase
+    {
+        private readonly FleetContext _context;
+
+        public BookingDetailsController(FleetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BookingDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookingDetail>>> GetBookingDetail()
+        {
+          if (_context.BookingDetail == null)
+          {
+              return NotFound();
+          }
+            return await _context.BookingDetail.ToListAsync();
+        }
+
+        // GET: api/BookingDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookingDetail>> GetBookingDetail(int? id)
+        {
+          if (_context.BookingDetail == null)
+          {
+              return NotFound();
+          }
+            var bookingDetail = await _context.BookingDetail.FindAsync(id);
+
+            if (bookingDetail == null)
+            {
+                return NotFound();
+            }
+
+            return bookingDetail;
+        }
+
+        // PUT: api/BookingDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookingDetail(int? id, BookingDetail bookingDetail)
+        {
+            if (id != bookingDetail.BookingDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bookingDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BookingDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BookingDetail>> PostBookingDetail(BookingDetail bookingDetail)
+        {
+          if (_context.BookingDetail == null)
+          {
+              return Problem("Entity set 'FleetContext.BookingDetail'  is null.");
+          }
+            _context.BookingDetail.Add(bookingDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBookingDetail", new { id = bookingDetail.BookingDetailId }, bookingDetail);
+        }
+
+        // DELETE: api/BookingDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookingDetail(int? id)
+        {
+            if (_context.BookingDetail == null)
+            {
+                return NotFound();
+            }
+            var bookingDetail = await _context.BookingDetail.FindAsync(id);
+            if (bookingDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.BookingDetail.Remove(bookingDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookingDetailExists(int? id)
+        {
+            return (_context.BookingDetail?.Any(e => e.BookingDetailId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Model/FleetContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FleetManagement.Model
+{
+    public class FleetContext : DbContext
+    {
+        public FleetContext() { }
+
+        public FleetContext(DbContextOptions<FleetContext> options) : base(options) { }
+
+
+
+        public DbSet<AddOnMaster> AddOnMaster { get; set; }
+        public DbSet<AirportMaster> AirportMaster { get; set; }
+
+        public DbSet<BookingDetail> BookingDetail { get; set; }
+        public DbSet<BookingHeader> BookingHeader { get; set; }
+        public DbSet<CarMaster> CarMaster { get; set; }
+        public DbSet<CarTypeMaster> CarTypeMaster { get; set; }
+        public DbSet<CityMaster> CityMaster { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<HubMaster> HubMaster { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetail { get; set; }
+        public DbSet<InvoiceHeader> InvoiceHeader { get; set; }
+        public DbSet<StateMaster> StateMaster { get; set; }
+
+    }
+}
